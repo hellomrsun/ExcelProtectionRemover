1dc840cd92ae1252b6030995a860f3350cd65a88
Jiangong SUN <jsun-ext@alpheys.com>
2021-10-03 11:07:41 +0200

    Initialize project

---
+++ ExcelProtectionRemover/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using ExcelProtectionRemover.Process;
+using Microsoft.Win32;
+
+namespace ExcelProtectionRemover
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void ChooseFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileDialog file = new OpenFileDialog();
+            var result = file.ShowDialog();
+            if (result.HasValue && !result.Value) return;
+            FileNameTbx.Text = file.FileName;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var fileFullPath = FileNameTbx.Text;
+            Task.Run(() =>
+            {
+                 var processor = new FileProcessor();
+                 var result = processor.Process(fileFullPath);
+            }).ContinueWith(x =>
+            {
+                MessageBox.Show("The file is converted!");
+            });
+        }
+    }
+}
---
+++ ExcelProtectionRemover/Process/FileProcessor.cs
@@ -0,0 +1,105 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExcelProtectionRemover.Process
+{
+    public class FileProcessor
+    {
+        public async Task<string> Process(string fileFullPath)
+        {
+            var convertedFileFullPath = CreateConvertedFileFullPath(fileFullPath);
+
+            CopyFileToConverted(fileFullPath, convertedFileFullPath);
+
+            var zipFileFullPath = RenameFileToZipFile(convertedFileFullPath);
+
+            var zipFolderName = ExtractZipFile(zipFileFullPath);
+
+            RemoveSheetProtection(zipFolderName, zipFileFullPath);
+
+            RemoveAllFilesAndFoldersFromFolder(zipFolderName);
+
+            convertedFileFullPath = Path.ChangeExtension(zipFileFullPath, ".xlsx");
+
+            RemoveFile(convertedFileFullPath);
+
+            File.Move(zipFileFullPath, convertedFileFullPath);
+
+            return convertedFileFullPath;
+        }
+
+        private static void RemoveSheetProtection(string zipFolderName, string zipFileFullPath)
+        {
+            var sheet1FilePath = Path.Combine(zipFolderName, @"xl\worksheets", "sheet1.xml");
+            var allContents = File.ReadAllText(sheet1FilePath);
+
+            var regex = new Regex("<sheetProtection[a-zA-Z0-9/ -=\"]+\\/>");
+            var newAllContents = regex.Replace(allContents, "");
+
+            File.WriteAllText(sheet1FilePath, newAllContents);
+
+            ZipFile.CreateFromDirectory(zipFolderName, zipFileFullPath);
+        }
+
+        private static string ExtractZipFile(string zipFileFullPath)
+        {
+            var zipFile = new FileInfo(zipFileFullPath);
+            var zipFileName = Path.GetFileNameWithoutExtension(zipFileFullPath);
+            var zipFolderName = Path.Combine(zipFile.DirectoryName, zipFileName);
+            RemoveAllFilesAndFoldersFromFolder(zipFolderName);
+
+            ZipFile.ExtractToDirectory(zipFileFullPath, zipFolderName);
+            File.Delete(zipFileFullPath);
+            return zipFolderName;
+        }
+
+        private static void RemoveAllFilesAndFoldersFromFolder(string zipFolderName)
+        {
+            if (Directory.Exists(zipFolderName))
+            {
+                var directory = new DirectoryInfo(zipFolderName);
+                foreach (var file in directory.GetFiles()) file.Delete();
+                foreach (var subDirectory in directory.GetDirectories()) subDirectory.Delete(true);
+                Directory.Delete(zipFolderName);
+            }
+        }
+
+        private static string RenameFileToZipFile(string convertedFileFullPath)
+        {
+            var zipFileFullPath = Path.ChangeExtension(convertedFileFullPath, ".zip");
+
+            RemoveFile(zipFileFullPath);
+
+            File.Move(convertedFileFullPath, zipFileFullPath);
+            return zipFileFullPath;
+        }
+
+        private static void RemoveFile(string zipFileFullPath)
+        {
+            if (File.Exists(zipFileFullPath))
+            {
+                File.Delete(zipFileFullPath);
+            }
+        }
+
+        private static string CreateConvertedFileFullPath(string fileFullPath)
+        {
+            var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileFullPath);
+            var newFileName = fileNameWithoutExtension + "_Converted";
+            var excelFile = new FileInfo(fileFullPath);
+            return Path.Combine(excelFile.DirectoryName, newFileName + excelFile.Extension);
+        }
+
+        private static void CopyFileToConverted(string oldFileFullPath, string newFileFullPath)
+        {
+            if (File.Exists(newFileFullPath))
+            {
+                File.Delete(newFileFullPath);
+            }
+
+            File.Copy(oldFileFullPath, newFileFullPath);
+        }
+    }
+}
